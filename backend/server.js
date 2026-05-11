0a36d430a440de6c1556bf31e3c1055821d554e9
Arpit <arpit@hos.com>
2026-05-11 08:54:03 +0530

    added vercel link in cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://hall-of-shame-nine.vercel.app',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
